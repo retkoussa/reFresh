0a9b5a98e371b1c2d57167222a1262526255a8a5
Firas Koussa <firaskoussauae@gmail.com>
2023-03-20 17:28:59 +0400

    Add naabu

--- reFresh.sh
+++ reFresh.sh
@@ -265,6 +265,7 @@ project_discovery(){
     go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
     go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
     go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+    go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 
 }
 
