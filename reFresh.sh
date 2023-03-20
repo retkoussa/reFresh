8cd29f9a6140944471bc1730154c4ffb7eb177fc
Firas Koussa <firaskoussauae@gmail.com>
2023-03-20 17:34:25 +0400

    Add katana from project discovery

--- reFresh.sh
+++ reFresh.sh
@@ -266,6 +266,7 @@ project_discovery(){
     go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
     go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
     go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
+    go install github.com/projectdiscovery/katana/cmd/katana@latest
 
 }
 
